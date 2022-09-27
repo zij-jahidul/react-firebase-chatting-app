b764951084a1b775ccb26f6e2cc6bea66a26b1f2
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2022-09-28 00:57:47 +0600

    react-chatting-app-for-react-setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes , Route } from "react-router-dom";
+import Login from './components/pages/login';
+import Registration from './components/pages/registration';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/registration" element={<Registration />}></Route>  
+        <Route path="/login" element={<Login />}></Route>  
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/pages/registration/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Registration() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Registration
+    </h1>
+    </div>
+  )
+}
+
+export default Registration
