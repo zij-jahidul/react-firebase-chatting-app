7c7598006f73bbf7742e4ff56993458a0cac0354
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2022-09-30 16:33:29 +0600

    registration-page-validation

--- src/components/pages/registration/index.js
+++ src/components/pages/registration/index.js
@@ -1,7 +1,120 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Registration() {
+  const [email, setEmail] = useState();
+  const [fullname, setFullname] = useState();
+  const [password, setPassword] = useState();
+
+  const [emailErr, setEmailErr] = useState();
+  const [fullnameErr, setFullnameErr] = useState();
+  const [passwordErr, setPasswordErr] = useState();
+
+  // password error for charecters
+  const [passAZ, setPassAZ] = useState(false);
+  const [passazz, setPassazz] = useState(false);
+  const [pass09, setPass09] = useState(false);
+  const [passsc, setPasssc] = useState(false);
+  const [pass8, setPass8] = useState(false);
+
+  // all password is correct
+  const [passCorrect, setPassCorrect] = useState(false);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setEmailErr("");
+  };
+  const handleFullName = (e) => {
+    setFullname(e.target.value);
+    setFullnameErr("");
+  };
+  const handlePassword = (e) => {
+    let count = 0;
+    // A-Z validation
+    if (/^(?=.*[A-Z])/.test(e.target.value)) {
+      setPassAZ(true);
+      count++;
+    } else {
+      setPassAZ(false);
+    }
+
+    // a-z validation
+    if (/^(?=.*[a-z])/.test(e.target.value)) {
+      setPassazz(true);
+      count++;
+    } else {
+      setPassazz(false);
+    }
+
+    // 0-9 validation
+    if (/^(?=.*[0-9])/.test(e.target.value)) {
+      setPass09(true);
+      count++;
+    } else {
+      setPass09(false);
+    }
+    // spacial charecter validation
+    if (/^(?=.*[!@#$%^&*])/.test(e.target.value)) {
+      setPasssc(true);
+      count++;
+    } else {
+      setPasssc(false);
+    }
+    // 8 charecter validtion
+    if (/^(?=.{8,})/.test(e.target.value)) {
+      setPass8(true);
+      count++;
+    } else {
+      setPass8(false);
+    }
+
+    // Correct Password
+    if (count == 5) {
+      setPassCorrect(true);
+      // password save from this;
+    } else {
+      setPassCorrect(false);
+    }
+    setPassword(e.target.value);
+    setPasswordErr("");
+  };
+
+  const handleSubmit = () => {
+    if (!email) {
+      setEmailErr("Email field is required");
+    } else {
+      if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        setEmailErr("Valid email is required.");
+      }
+    }
+
+    if (!fullname) {
+      setFullnameErr("Full Name field is required");
+    } else {
+      if (fullname.length <= 3 || fullname.length >= 20) {
+        setFullnameErr("Your full name must be between 4 to 20 characters.");
+      }
+    }
+
+    if (!password) {
+      setPasswordErr("Password Field is required.");
+    } else {
+      if (!/^(?=.*[A-Z])/.test(password)) {
+        setPasswordErr("Password must be 1 capital letter.");
+      } else if (!/^(?=.*[a-z])/.test(password)) {
+        setPasswordErr("Password must be 1 small letter.");
+      } else if (!/^(?=.*[0-9])/.test(password)) {
+        setPasswordErr("Password must be 1 digit.");
+      } else if (!/^(?=.*[!@#$%^&*])/.test(password)) {
+        setPasswordErr("Password must be 1 special character.");
+      } else if (!/^(?=.{8,})/.test(password)) {
+        setPasswordErr("Password must be eight characters or longer.");
+      } else {
+        console.log("Correct Password");
+      }
+    }
+  };
+
   return (
     <div className="sm:flex">
       <div className="md:w-1/2 flex flex-col items-center md:items-end md:mr-[69px] mx-[30px] justify-center">
@@ -14,51 +127,101 @@ function Registration() {
           </p>
 
           <div className="w-[320px] md:w-[420px] mt-3">
-            <form>
-              <div className="relative">
-                <p className="font-nunito text-sm font-semibold bg-white text-textcolor px-2.5 absolute top-6 left-8">
-                  Email Address
-                </p>
-                <input
-                  type="email"
-                  className="border-solid w-full py-6 border-[#B8B9CE] border mt-9 rounded-lg px-10 placeholder:text-inputcolor placeholder:font-semibold focus:outline-none"
-                  placeholder="Enter Email"
-                />
-              </div>
-
-              <div className="relative">
-                <p className="font-nunito text-sm font-semibold bg-white text-textcolor px-2.5 absolute top-6 left-8">
-                  Full Name
-                </p>
-                <input
-                  type="email"
-                  className="border-solid w-full py-6 border-[#B8B9CE] border mt-9 rounded-lg px-10 placeholder:text-inputcolor placeholder:font-semibold focus:outline-none"
-                  placeholder="Enter Full Name"
-                />
-              </div>
-
-              <div className="relative">
-                <p className="font-nunito text-sm font-semibold bg-white text-textcolor px-2.5 absolute top-6 left-8">
-                  Password
-                </p>
-                <input
-                  type="password"
-                  className="border-solid w-full py-6 border-[#B8B9CE] border mt-9 rounded-lg px-10 placeholder:text-inputcolor placeholder:font-semibold focus:outline-none"
-                  placeholder="Enter Password"
-                />
-              </div>
-
-              <button className="w-full text-center bg-primary py-5 font-nunito font-semibold text-xl text-white mt-10 rounded-full">
-                Sign up
-              </button>
-
-              <p className="font-nunito mt-7 w-full font-normal text-sm text-center">
-                Already have an account ?
-                <Link className="font-bold text-[#EA6C00] ml-2" to="/login">
-                  Sign In
-                </Link>
+            <div className="relative">
+              <p className="font-nunito text-sm font-semibold bg-white text-textcolor px-2.5 absolute top-6 left-8">
+                Email Address
               </p>
-            </form>
+              <input
+                type="email"
+                className="border-solid w-full py-6 border-[#B8B9CE] border mt-9 rounded-lg px-10 placeholder:text-inputcolor placeholder:font-semibold focus:outline-none"
+                placeholder="Enter Email"
+                onChange={handleEmail}
+              />
+
+              {emailErr && (
+                <p className="text-red-500 font-nunito font-normal pl-3 mt-1">
+                  {emailErr}
+                </p>
+              )}
+            </div>
+
+            <div className="relative">
+              <p className="font-nunito text-sm font-semibold bg-white text-textcolor px-2.5 absolute top-6 left-8">
+                Full Name
+              </p>
+              <input
+                type="email"
+                className="border-solid w-full py-6 border-[#B8B9CE] border mt-9 rounded-lg px-10 placeholder:text-inputcolor placeholder:font-semibold focus:outline-none"
+                placeholder="Enter Full Name"
+                onChange={handleFullName}
+              />
+
+              {fullnameErr && (
+                <p className="text-red-500 font-nunito font-normal pl-3 mt-1">
+                  {fullnameErr}
+                </p>
+              )}
+            </div>
+
+            <div className="relative">
+              <p className="font-nunito text-sm font-semibold bg-white text-textcolor px-2.5 absolute top-6 left-8">
+                Password
+              </p>
+              <input
+                type="text"
+                className="border-solid w-full py-6 border-[#B8B9CE] border mt-9 rounded-lg px-10 placeholder:text-inputcolor placeholder:font-semibold focus:outline-none"
+                placeholder="Enter Password"
+                onChange={handlePassword}
+              />
+
+              {passwordErr && (
+                <p className="text-red-500 font-nunito font-normal pl-3 mt-1">
+                  {passwordErr}
+                </p>
+              )}
+
+              {passCorrect ? (
+                <p className="text-green-500 font-nunito font-normal pl-3 mt-1">
+                  Correct & Strong Password
+                </p>
+              ) : (
+                <p className="font-nunito text-sm pl-3 mt-1">
+                  <span className={passAZ ? "text-green-500" : "text-red-500"}>
+                    [A-Z]
+                  </span>{" "}
+                  ,
+                  <span className={passazz ? "text-green-500" : "text-red-500"}>
+                    [a-z]
+                  </span>{" "}
+                  ,
+                  <span className={pass09 ? "text-green-500" : "text-red-500"}>
+                    [0-9]
+                  </span>{" "}
+                  ,
+                  <span className={passsc ? "text-green-500" : "text-red-500"}>
+                    [Spcial character]
+                  </span>{" "}
+                  ,
+                  <span className={pass8 ? "text-green-500" : "text-red-500"}>
+                    [8 characters]
+                  </span>{" "}
+                </p>
+              )}
+            </div>
+
+            <button
+              onClick={handleSubmit}
+              className="w-full text-center bg-primary py-5 font-nunito font-semibold text-xl text-white mt-10 rounded-full"
+            >
+              Sign up
+            </button>
+
+            <p className="font-nunito mt-7 w-full font-normal text-sm text-center">
+              Already have an account ?
+              <Link className="font-bold text-[#EA6C00] ml-2" to="/login">
+                Sign In
+              </Link>
+            </p>
           </div>
         </div>
       </div>
