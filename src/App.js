eb2d3e05352128acdbfd93ca24edea7d56137cdf
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2022-09-30 18:49:20 +0600

    route-change-form-registration-page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Routes , Route } from "react-router-dom";
-import Login from './components/pages/login';
-import Registration from './components/pages/registration';
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/pages/login";
+import Registration from "./components/pages/registration";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/registration" element={<Registration />}></Route>  
-        <Route path="/login" element={<Login />}></Route>  
+        <Route path="/" element={<Registration />}></Route>
+        <Route path="/registration" element={<Registration />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </>
   );
