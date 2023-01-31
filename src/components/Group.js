31f3f5467a9e0d25c8013abe4722873b0d4cf7ca
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-01-31 22:11:25 +0600

    Group List & Create Group

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,121 +1,122 @@
-import React from "react";
-import { BiDotsVerticalRounded } from "react-icons/bi";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+  push,
+  remove,
+} from "firebase/database";
+import { getAuth } from "firebase/auth";
 const Group = () => {
+  let db = getDatabase();
+  let auth = getAuth();
+  let [cgroup, setCgroup] = useState(false);
+  let [gname, setGname] = useState("");
+  let [gtag, setGtag] = useState("");
+  let [grouplist, setGrouplist] = useState([]);
+
+  let handleCreateGroup = () => {
+    set(push(ref(db, "group")), {
+      gname: gname,
+      gtag: gtag,
+      adminname: auth.currentUser.displayName,
+      adminid: auth.currentUser.uid,
+    }).then(() => {
+      setCgroup(false);
+    });
+  };
+
+  useEffect(() => {
+    const usersRef = ref(db, "group");
+    onValue(usersRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (item.val().adminid != auth.currentUser.uid) {
+          arr.push({ ...item.val(), gid: item.key });
+        }
+      });
+      setGrouplist(arr);
+    });
+  }, []);
+
+  let handleGroupJoin = (item) => {
+    set(push(ref(db, "groupjoinrequest")), {
+      adminid: item.adminid,
+      gid: item.gid,
+      gname: item.gname,
+      gtag: item.gtag,
+      userid: auth.currentUser.uid,
+      username: auth.currentUser.displayName,
+      userprofile: auth.currentUser.photoURL,
+    });
+    set(push(ref(db, "notification")), {
+      adminid: item.adminid,
+      gid: item.gid,
+      gname: item.gname,
+      gtag: item.gtag,
+      userid: auth.currentUser.uid,
+      username: auth.currentUser.displayName,
+    });
+  };
+
   return (
-    <div className="shadow-lg shadow-black-500/50 p-5 h-[370px] overflow-y-auto scrollbar-hide rounded-3xl mt-5">
-      <div className="flex justify-between">
-        <h3 className="font-nunito font-semibold text-xl">
-          Groups Request
-        </h3>
+    <div className="shadow-sm shadow-black p-5 h-[427px] overflow-y-scroll rounded-3xl mt-5">
+      <h3 className="font-nunito font-semibold text-xl flex justify-between">
+        Groups List{" "}
+        <button
+          onClick={() => setCgroup(!cgroup)}
+          className="font-nunito font-bold text-lg text-white bg-primary p-1.5 rounded"
+        >
+          {cgroup ? "Go back" : "Create Group"}
+        </button>
+      </h3>
 
-        <span>
-          <Link to="#">
-            <BiDotsVerticalRounded className="font-nunito font-semibold text-[28px] cursor-pointer text-primary" />
-          </Link>
-        </span>
-      </div>
+      {cgroup ? (
+        <>
+          <input
+            className="border border-solid border-black w-full rounded-lg px-14 py-6 sml:p-4  md:!py-3 mt-9 sml:mt-4 md:!mt-9"
+            placeholder="Group Name"
+            onChange={(e) => setGname(e.target.value)}
+          />
+          <input
+            className="border border-solid border-black w-full rounded-lg px-14 py-6 sml:p-4  md:!py-3 mt-9 sml:mt-4 md:!mt-3"
+            placeholder="Group Tagline"
+            onChange={(e) => setGtag(e.target.value)}
+          />
+          <button
+            onClick={handleCreateGroup}
+            className="w-full text-center bg-primary rounded-[86px] py-2 font-nunito font-semibold text-xl text-white mt-12 sml:mt-4 md:!mt-3"
+          >
+            Create Group
+          </button>
+        </>
+      ) : (
+        grouplist.map((item) => (
+          <div className="flex justify-between items-center border-b border-solid border-black pb-2.5 m-5">
+            <img
+              src="images/profileimg.png"
+              className="w-[70px] h-[70px] rounded"
+            />
+            <div>
+              <span className="text-sm">Admin:{item.adminname}</span>
+              <h3 className="font-nunito font-semibold text-lg">
+                {item.gname}
+              </h3>
 
-      <div className="flex justify-between items-center border-b border-solid pb-2.5 m-5">
-        <div className="flex items-center gap-5">
-          <picture>
-            <img
-              src="images/profileimg.png"
-              alt="profile-01"
-              className="w-[70px] h-[70px] rounded"
-            />
-          </picture>
-          <div>
-            <h3 className="font-nunito font-bold text-lg">Friends Reunion</h3>
-            <p className="font-nunito font-normal text-sm text-[#4D4D4D]">Hi Guys, Wassup!</p>
+              <p className="font-nunito font-semibold text-sm">{item.gtag}</p>
+            </div>
+            <div>
+              <button
+                onClick={() => handleGroupJoin(item)}
+                className="font-nunito font-bold text-lg text-white bg-primary p-1.5 rounded"
+              >
+                Join
+              </button>
+            </div>
           </div>
-        </div>
-        <div>
-          <button className="font-nunito font-bold text-md text-white bg-primary p-1.5 rounded">
-            Accpet
-          </button>
-        </div>
-      </div>
-      <div className="flex justify-between items-center border-b border-solid pb-2.5 m-5">
-        <div className="flex items-center gap-5">
-          <picture>
-            <img
-              src="images/profileimg.png"
-              alt="profile-01"
-              className="w-[70px] h-[70px] rounded"
-            />
-          </picture>
-          <div>
-            <h3 className="font-nunito font-bold text-lg">Friends Reunion</h3>
-            <p className="font-nunito font-normal text-sm text-[#4D4D4D]">Hi Guys, Wassup!</p>
-          </div>
-        </div>
-        <div>
-          <button className="font-nunito font-bold text-md text-white bg-primary p-1.5 rounded">
-            Accpet
-          </button>
-        </div>
-      </div>
-      <div className="flex justify-between items-center border-b border-solid pb-2.5 m-5">
-        <div className="flex items-center gap-5">
-          <picture>
-            <img
-              src="images/profileimg.png"
-              alt="profile-01"
-              className="w-[70px] h-[70px] rounded"
-            />
-          </picture>
-          <div>
-            <h3 className="font-nunito font-bold text-lg">Friends Reunion</h3>
-            <p className="font-nunito font-normal text-sm text-[#4D4D4D]">Hi Guys, Wassup!</p>
-          </div>
-        </div>
-        <div>
-          <button className="font-nunito font-bold text-md text-white bg-primary p-1.5 rounded">
-            Accpet
-          </button>
-        </div>
-      </div>
-      <div className="flex justify-between items-center border-b border-solid pb-2.5 m-5">
-        <div className="flex items-center gap-5">
-          <picture>
-            <img
-              src="images/profileimg.png"
-              alt="profile-01"
-              className="w-[70px] h-[70px] rounded"
-            />
-          </picture>
-          <div>
-            <h3 className="font-nunito font-bold text-lg">Friends Reunion</h3>
-            <p className="font-nunito font-normal text-sm text-[#4D4D4D]">Hi Guys, Wassup!</p>
-          </div>
-        </div>
-        <div>
-          <button className="font-nunito font-bold text-md text-white bg-primary p-1.5 rounded">
-            Accpet
-          </button>
-        </div>
-      </div>
-      <div className="flex justify-between items-center border-b border-solid pb-2.5 m-5 last:border-0">
-        <div className="flex items-center gap-5">
-          <picture>
-            <img
-              src="images/profileimg.png"
-              alt="profile-01"
-              className="w-[70px] h-[70px] rounded"
-            />
-          </picture>
-          <div>
-            <h3 className="font-nunito font-bold text-lg">Friends Reunion</h3>
-            <p className="font-nunito font-normal text-sm text-[#4D4D4D]">Hi Guys, Wassup!</p>
-          </div>
-        </div>
-        <div>
-          <button className="font-nunito font-bold text-md text-white bg-primary p-1.5 rounded">
-            Accpet
-          </button>
-        </div>
-      </div>
+        ))
+      )}
     </div>
   );
 };
